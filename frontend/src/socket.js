34eee7c6de17a7d0beb3257a6f46ced86eea9993
Yeab <yeabtesfayetesfaye519@gmail.com>
2025-02-11 16:25:23 +0300

    - fixed socket error

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = '/'; // Backend server URL
+const SOCKET_URL = 'https://yeab-chat.onrender.com'; // Backend server URL
 
 const socket = io(SOCKET_URL, {
     autoConnect: false, // Connect manually when needed
